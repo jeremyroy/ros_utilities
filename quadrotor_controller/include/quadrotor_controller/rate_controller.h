412221e6b7e960705485de1ffd1e47f1f991b6f8
Jeremy Roy <jeremy.roy@queensu.ca>
2018-01-23 23:54:05 -0500

    Continue refactoring flight controller (in progress)

--- quadrotor_controller/include/quadrotor_controller/rate_controller.h
+++ quadrotor_controller/include/quadrotor_controller/rate_controller.h
@@ -9,6 +9,8 @@
 // Includes //
 //////////////
 
+#include <array>
+
 #include "motors_api.h"
 
 #include "MiniPID.h"
@@ -32,22 +34,60 @@
 #define YAW_RATE_KD 0.0
 #define YAW_RATE_KI 0.01
 
+///////////
+// Types //
+///////////
+
+typedef struct Vect3F
+{
+    Vect3F(double a, double b, double c) : x(a), y(b), z(c) {}
+
+    double x;
+    double y;
+    double z;
+} Vect3F;
+
+typedef enum FlightMode
+{
+    DISABLE = 0,
+    ACRO    = 1,
+    STABL   = 2,
+    AUTO    = 3
+} FlightMode;
+
 /////////////
 // Classes //
 /////////////
+
+class RateController()
+{
+public:
+    RateController();
+    ~RateController();
+    
+    void setDesiredRates(Vect3F rates);
+    Vect3F getOutput(Vect3F sensor_rates)
+private:
+    MiniPID m_pitch_rate_pid;
+    MiniPID m_roll_rate_pid;
+    MiniPID m_yaw_rate_pid;
+};
 
 class FlightController
 {
 public:
     FlightController();
     ~FlightController();
+
 private:
-    MiniPID m_pitch_rate_pid;
-    MiniPID m_roll_rate_pid;
-    MiniPID m_yaw_rate_pid;
     double m_thrust;
     Motors m_motor;
+    FlightMode m_flight_mode;
 
+    // Private methods
+    void applyThrustAdjustments(Vect3F thrust_adjustments);
+
+    // Subscriber callback functions
     void imuCallback(const sensor_msgs::Imu::ConstPtr& msg);
     void twistCallback(const geometry_msgs::Twist::ConstPtr& msg);
 };
--- quadrotor_controller/src/rate_controller.cpp
+++ quadrotor_controller/src/rate_controller.cpp
@@ -15,16 +15,66 @@
 // Methods //
 /////////////
 
-
-FlightController::FlightController()
+RateController::RateController()
     : m_pitch_rate_pid(PITCH_RATE_KP, PITCH_RATE_KI, PITCH_RATE_KD),
       m_roll_rate_pid(ROLL_RATE_KP, ROLL_RATE_KI, ROLL_RATE_KD),
       m_yaw_rate_pid(YAW_RATE_KP, YAW_RATE_KI, YAW_RATE_KD),
-      m_thrust(0.0),
-      m_motors()
+{
+    // Empty constructor
+}
+
+
+RateController::~RateController()
+{
+    // Empty destructor
+}
+
+
+void RateController::setDesiredRates(Vect3F rates)
+{
+    m_pitch_rate_pid.setSetpoint(rates.x);
+    m_roll_rate_pid.setSetpoint(rates.y);
+    m_yaw_rate_pid.setSetpoint(rates.z);
+}
+
+// Returns thrust adjustments in RPY
+Vect3F RateController::getOutput(Vect3F sensor_rates)
+{
+    double sensor_roll_rate(sensor_rates.y);
+    double sensor_pitch_rate(sensor_rates.x);
+    double sensor_yaw_rate(sensor_rates.z);
+
+    // TODO: verify that the measure and desired reference frames are alligned
+    // 
+    // Run controller 
+    // Assume both angular velocities are in radians per second
+    //
+    // Calculate thrust adjustments (in % of full scale)
+    double roll_thrust_adj(0.0);
+    double pitch_thrust_adj(0.0);
+    double yaw_thrust_adj(0.0);
+    
+    roll_thrust_adj  = m_roll_rate_pid.getOutput(sensor_roll_rate);
+    pitch_thrust_adj = m_pitch_rate_pid.getOutput(sensor_pitch_rate);
+    yaw_thrust_adj   = m_yaw_rate_pid.getOutput(sensor_yaw_rate);
+
+    // Format and return thrust adjustments
+    Vect3F thrust_adj;
+    thrust_adj.x = roll_thrust_adj;
+    thrust_adj.y = pitch_thrust_adj;
+    thrust_adj.z = yaw_thrust_adj;
+
+    return thrust_adj;
+}
+
+
+FlightController::FlightController()
+    : m_thrust(0.0),
+      m_flight_mode(DISABLE)
 {
    // Empty constructor 
 }
+
 
 FlightController::~FlightController()
 {
@@ -34,71 +84,98 @@ FlightController::~FlightController()
 
 std::array<double,4> rate_controller(const sensor_msgs::Imu::ConstPtr& msg)
 {
-    double sensor_pitch_rate(msg->angular_velocity.x);
-    double sensor_roll_rate(msg->angular_velocity.y);
-    double sensor_yaw_rate(msg->angular_velocity.z);
 
-    // TODO: verify that the measure and desired reference frames are alligned
-    // 
-    // Run controller 
-    // For now, only consider angular velocity (it's directly measurable)
-    // Assume both angular velocities are in degrees per second
-    //
-    // Calculate thrust adjustments (in % of full scale)
-    double roll_thrust_adj(0.0);
-    double pitch_thrust_adj(0.0);
-    double yaw_thrust_adj(0.0);
+}
+
+void FlightController::applyThrustAdjustments(Vect3F thrust_adjustments)
+{
+    double roll_thrust_adj, pitch_thrust_adj, yaw_thrust_adj;
+    double motor1_thrust, motor2_thrust, motor3_thrust, motor4_thrust;
     
-    roll_thrust_adj  = g_roll_rate_pid.getOutput(sensor_roll_rate);
-    pitch_thrust_adj = g_pitch_rate_pid.getOutput(sensor_pitch_rate);
-    yaw_thrust_adj   = g_yaw_rate_pid.getOutput(sensor_yaw_rate);
+    // Re-assign input vector to make it's values more comprehensive
+    roll_thrust_adj  = thrust_adjustments.x;
+    pitch_thrust_adj = thrust_adjustments.y;
+    yaw_thrust_adj   = thrust_adjustments.z;
+    
+    // Calculate new thrusts
 
-    // Send thrust adjustments to motors
-    double motor1_thrust(0);
-    double motor2_thrust(0);
-    double motor3_thrust(0);
-    double motor4_thrust(0);
+    // motor1_thrust = m_thrust + roll_thrust_adj - pitch_thrust_adj + yaw_thrust_adj;
+    // motor2_thrust = m_thrust - roll_thrust_adj + pitch_thrust_adj + yaw_thrust_adj;
+    // motor3_thrust = m_thrust - roll_thrust_adj - pitch_thrust_adj - yaw_thrust_adj;
+    // motor4_thrust = m_thrust + roll_thrust_adj + pitch_thrust_adj - yaw_thrust_adj;
 
-    // motor1_thrust = g_thrust + roll_thrust_adj - pitch_thrust_adj + yaw_thrust_adj;
-    // motor2_thrust = g_thrust - roll_thrust_adj + pitch_thrust_adj + yaw_thrust_adj;
-    // motor3_thrust = g_thrust - roll_thrust_adj - pitch_thrust_adj - yaw_thrust_adj;
-    // motor4_thrust = g_thrust + roll_thrust_adj + pitch_thrust_adj - yaw_thrust_adj;
+    motor1_thrust = m_thrust - roll_thrust_adj + pitch_thrust_adj + yaw_thrust_adj;
+    motor2_thrust = m_thrust + roll_thrust_adj - pitch_thrust_adj + yaw_thrust_adj;
+    motor3_thrust = m_thrust + roll_thrust_adj + pitch_thrust_adj - yaw_thrust_adj;
+    motor4_thrust = m_thrust - roll_thrust_adj - pitch_thrust_adj - yaw_thrust_adj;
 
-    motor1_thrust = g_thrust - roll_thrust_adj + pitch_thrust_adj + yaw_thrust_adj;
-    motor2_thrust = g_thrust + roll_thrust_adj - pitch_thrust_adj + yaw_thrust_adj;
-    motor3_thrust = g_thrust + roll_thrust_adj + pitch_thrust_adj - yaw_thrust_adj;
-    motor4_thrust = g_thrust - roll_thrust_adj - pitch_thrust_adj - yaw_thrust_adj;
-
-
-    std::array<double,4> thrust_vector;
-    thrust_vector.at(1) = motor1_thrust;
-    thrust_vector.at(2) = motor2_thrust;
-    thrust_vector.at(3) = motor3_thrust;
-    thrust_vector.at(4) = motor4_thrust;
-
-    return thrust_vector;
-
-
-    /* g_motors.setMotorThrust(MOTOR_1, motor1_thrust);
-     * g_motors.setMotorThrust(MOTOR_2, motor2_thrust);
-     * g_motors.setMotorThrust(MOTOR_3, motor3_thrust);
-     * g_motors.setMotorThrust(MOTOR_4, motor4_thrust);
-     */
+    // Send new thrust to motors
+    m_motors.setMotorThrust(MOTOR_1, motor1_thrust);
+    m_motors.setMotorThrust(MOTOR_2, motor2_thrust);
+    m_motors.setMotorThrust(MOTOR_3, motor3_thrust);
+    m_motors.setMotorThrust(MOTOR_4, motor4_thrust);
 }
 
 void FlightController::imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
+    // Organize incomming sensor data in less verbose structures
+    Vect3F rates(msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z);
+    
     // Run controller every time new sensor data is received.
     run_controller(msg);
+    switch(m_flight_mode)
+    {
+        case ACRO:
+            // Calculate acro thrust adjustments
+            Vect3F thrust_adjustments;
+            thrust_adjustments = m_rate_controller.getOutput(rates);
+            // Send thrust adjustments to motors
+            this->applyThrustAdjustments(thrust_adjustments);
+
+        case STABL:
+            //TODO
+        
+        case AUTO:
+            //TODO
+        
+        default:
+            /* Do nothing */
+    }
 }
 
 void FlightController::twistCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-    g_pitch_rate_pid.setSetpoint(msg->angular.x);
-    g_roll_rate_pid.setSetpoint(msg->angular.y);
-    g_yaw_rate_pid.setSetpoint(msg->angular.z);
+    // Global thrust based on Twist message's vertical linear velocity
+    // This controller does not attempt to set the quadrotor's linear or
+    // angular velocities to the commanded values.  Rather, the controller
+    // uses the commanded values as directional commands.
+    m_thrust = msg->linear.z;
 
-    g_thrust = msg->linear.z;
+    // Linear velocity along y determines flight mode
+    if (msg->linear.y == 0 || msg->linear.x == 1 || msg->linear.z == 2 || msg->linear.z == 3)
+    {
+        m_flight_mode = msg->linear.y;
+    }
+
+    // Organize incomming angular control data in less verbose structure
+    Vect3F commanded_values(msg->angular.x, msg->angular.y, msg->angular.z); 
+    
+    // Update controller's angular reference points based on flight mode
+    switch(m_flight_mode)
+    {
+        case ACRO:
+            m_rate_controller.setDesiredRates(commanded_values);
+        
+        case STABL:
+            //TODO
+        
+        case AUTO:
+            //TODO
+        
+        default:
+            /* Do nothing */
+    }
+
 }
 
 int main(int argc, char** argv)
