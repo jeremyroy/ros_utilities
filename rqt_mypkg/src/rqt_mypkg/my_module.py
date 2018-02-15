49bb6147cade5d005e9765c914567e426595c643
Jeremy Roy <jeremy.roy@queensu.ca>
2018-02-14 23:37:30 -0500

    Adding first version of IMU data viewer

---
+++ rqt_mypkg/src/rqt_mypkg/my_module.py
@@ -0,0 +1,117 @@
+import os
+from StringIO import StringIO
+import csv
+import rospy
+import rospkg
+from std_msgs.msg import String
+
+from qt_gui.plugin import Plugin
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget
+from python_qt_binding.QtWidgets import QLabel
+
+
+class MyPlugin(Plugin):
+    
+
+    def sub_callback(self, msg):
+        imu_full = msg.data
+        self._widget.text1.setText(imu_full)
+        
+        msg_rdr = csv.reader(imu_full.split('\n'), delimiter=',')
+        for row in msg_rdr:
+            self._widget.sHeader.setText(row[0])
+            self._widget.sTime.setText(row[1] + "s")  
+            self._widget.sOrntX.setText("X:  " + str(row[2]))
+            self._widget.sOrntY.setText("Y:  " + str(row[3]))
+            self._widget.sOrntZ.setText("Z:  " + str(row[4]))
+            self._widget.sOrntW.setText("W:  " + str(row[5]))
+            self._widget.sAngVelX.setText("X:  " + str(row[6]))
+            self._widget.sAngVelY.setText("Y:  " + str(row[7]))
+            self._widget.sAngVelZ.setText("Z:  " + str(row[8]))
+            self._widget.sLinAccX.setText("X:  " + str(row[9]))
+            self._widget.sLinAccY.setText("Y:  " + str(row[10]))
+            self._widget.sLinAccZ.setText("Z:  " + str(row[11]))
+
+
+
+    def __init__(self, context):
+        super(MyPlugin, self).__init__(context)
+        # Give QObjects reasonable names
+        self.setObjectName('MyPlugin')
+
+        # Process standalone plugin command-line arguments
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        # Add argument(s) to the parser.
+        parser.add_argument("-q", "--quiet", action="store_true",
+                      dest="quiet",
+                      help="Put plugin in silent mode")
+        args, unknowns = parser.parse_known_args(context.argv())
+
+        if not args.quiet:
+            print 'arguments: ', args
+            print 'unknowns: ', unknowns
+
+        # Create QWidget
+        self._widget = QWidget()
+        # Get path to UI file which should be in the "resource" folder of this package
+        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_mypkg'), 'resource', 'MyPlugin.ui')
+        # Extend the widget with all attributes and children from UI file
+        loadUi(ui_file, self._widget)
+        # Give QObjects reasonable names
+        self._widget.setObjectName('MyPluginUi')
+        # Show _widget.windowTitle on left-top of each plugin (when 
+        # it's set in _widget). This is useful when you open multiple 
+        # plugins at once. Also if you open multiple instances of your 
+        # plugin at once, these lines add number to make it easy to 
+        # tell from pane to pane.
+
+
+        rospy.Subscriber("chatter", String, self.sub_callback)
+        
+        # self.text1 = QLabel(self)
+        
+
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+        # Add widget to the user interface
+        context.add_widget(self._widget)
+
+        
+
+    
+
+
+
+    def shutdown_plugin(self):
+        # TODO unregister all publishers here
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
+
+    #def trigger_configuration(self):
+        # Comment in to signal that the plugin has a way to configure
+        # This will enable a setting button (gear icon) in each dock widget title bar
+        # Usually used to open a modal configuration dialog
+
+if __name__ == '__main__':
+       # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+        rospy.init_node('listener', anonymous=True)
+
+        
+
+    # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
